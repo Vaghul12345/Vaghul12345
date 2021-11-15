ee633d5c0f101cc69255340ad7966b48d3cc5b19
Vaghul12345 <89490846+Vaghul12345@users.noreply.github.com>
2021-11-15 14:06:30 -0800

    Update shifter.v

--- shifter.v
+++ shifter.v
@@ -4,12 +4,15 @@ module shifter(shift, in, out)
   input [15:0] in;
   output [15:0] out;
   
+  
+  //Shift the input either left or right by one, or keep it equal
   always@(*)
     casex(shift)
       2'b00: out = in;
       2'b01: out = in<<1;
       2'b10: out = in>>1;
-      2'b11: out = 
+      2'b11: out = {in[15], in[15:1]);
+           
       default out = 16'bxxxxxxxxxxxxxxxx;
     endcase
   end
