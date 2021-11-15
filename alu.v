e783c7aa293a374d4f1906dd53eaf8c962f311b3
Vaghul12345 <89490846+Vaghul12345@users.noreply.github.com>
2021-11-15 13:54:32 -0800

    Update alu.v

--- alu.v
+++ alu.v
@@ -4,13 +4,20 @@ module alu(Ain,Bin,ALUop,out,Z);
   output [15:0] out;
   output Z;
 
+  //Always block for each case of ALU operation
   always @(*)
     casex(ALUop)
       2'b00: out = Ain+Bin;
       2'b01  out = Ain-Bin;
       2'b10  out = Ain & Bin;
       2'b11  out = ~Bin;
-      default out = 16'b0000000000000000;
+      default out = 16'bxxxxxxxxxxxxxxxx;
     endcase
+  
+  //Special case, if out is 0, then Z is 1
+  if(out == 16'b0000000000000000)
+    Z = 1'b1;
+  else
+    Z = 1'b0;
   end
 endmodule
