53c8831d2eea9b8059ecd7b669d408fdcd66a6f9
Vaghul12345 <89490846+Vaghul12345@users.noreply.github.com>
2021-11-15 18:15:01 -0800

    Create regfile.v

--- regfile.v
+++ regfile.v
@@ -26,14 +26,15 @@ wire [15:0]  R0,R1,R2,R3,R4,R5,R6,R7;
 //Register Load
 module vDFFE(clk, en, in,out)
 parameter n = 16;
-input clk, en;
+input clk, en; // instantiate clk and load
 input [n-1:0] in;
 output [n-1:0] out;
 reg [n-1:0] out;
 wire [n-1:0] next_out;
-assign next_out = en ? in : out;
+assign next_out = en ? in : out; // assign next
 always @ (posedge clk)
 out = next_out;
+
 endmodule
   
 //Decoder
@@ -58,6 +59,5 @@ endmodule
   endmodule  
 
 
-
-
+end
 endmodule
