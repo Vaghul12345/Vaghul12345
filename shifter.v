4269b99fc0afdd2635b4ce7021bbf7ed0fa47f28
Vaghul12345 <89490846+Vaghul12345@users.noreply.github.com>
2021-11-15 17:34:02 -0800

    Update shifter.v

--- shifter.v
+++ shifter.v
@@ -1,4 +1,4 @@
-module shifter(shift, in, sout)
+module shifter(shift, in, sout);
   
   input [1:0] shift;
   input [15:0] in;
@@ -11,7 +11,7 @@ module shifter(shift, in, sout)
       2'b00: sout = in;
       2'b01: sout = in<<1;
       2'b10: sout = in>>1;
-      2'b11: sout = {in[15], in[15:1]);
+      2'b11: sout = {in[15], in[15:1]};
            
       default sout = 16'bxxxxxxxxxxxxxxxx;
     endcase
