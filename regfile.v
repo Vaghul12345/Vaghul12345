e878cd0939a15917ffd4ed19cde8e33cd65aa48a
Vaghul12345 <89490846+Vaghul12345@users.noreply.github.com>
2021-11-15 18:17:01 -0800

    Update regfile.v

--- regfile.v
+++ regfile.v
@@ -12,19 +12,19 @@ wire [15:0]  R0,R1,R2,R3,R4,R5,R6,R7;
   Dec #(3,8) U1(writenum,writenumoutput);
   Dec #(3,8) U2(readnum,readnumoutput);
   
-  vDFF #(16) RR0 (clk, en[0], datain, R0);
-  vDFF #(16) RR1 (clk, en[1], datain, R1);
-  vDFF #(16) RR2 (clk, en[2], datain, R2);
-  vDFF #(16) RR3 (clk, en[3], datain, R3);
-  vDFF #(16) RR4 (clk, en[4], datain, R4);
-  vDFF #(16) RR5 (clk, en[5], datain, R5);
-  vDFF #(16) RR6 (clk, en[6], datain, R6);
-  vDFF #(16) RR7 (clk, en[7], datain, R7);
+  vDFFE #(16) RR0 (clk, en[0], data_in, R0);
+  vDFFE #(16) RR1 (clk, en[1], data_in, R1);
+  vDFFE #(16) RR2 (clk, en[2], data_in, R2);
+  vDFFE #(16) RR3 (clk, en[3], data_in, R3);
+  vDFFE #(16) RR4 (clk, en[4], data_in, R4);
+  vDFFE #(16) RR5 (clk, en[5], data_in, R5);
+  vDFFE #(16) RR6 (clk, en[6], data_in, R6);
+  vDFFE #(16) RR7 (clk, en[7], data_in, R7);
   
   Mux8 #(16) U3 (R7,R6,R5,R4,R3,R2,R1,R0,readnumoutput, data_out); 
   
 //Register Load
-module vDFFE(clk, en, in,out)
+module vDFFE(clk, en, in,out);
 parameter n = 16;
 input clk, en; // instantiate clk and load
 input [n-1:0] in;
@@ -38,7 +38,7 @@ out = next_out;
 endmodule
   
 //Decoder
-module Dec(a,b)
+module Dec(a,b);
 parameter n =3 ;
 parameter m =8;
   input [n-1:0] a;
@@ -48,7 +48,7 @@ endmodule
   
   
   //8-input, k-bit mux with one hot select
-  module Mux8(a7,a6,a5,a4,a3,a1,a0,s,b);
+  module Mux8(a7,a6,a5,a4,a3,a2,a1,a0,s,b);
   parameter k = 16;
     input [k-1:0] a0, a1, a2, a3,a4, a5, a6, a7; // inputs
     input [7:0] s; // one-hot select
@@ -59,5 +59,5 @@ endmodule
   endmodule  
 
 
-end
+
 endmodule
